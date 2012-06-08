664a3e1f3e6656dfefa302555928256478d9827e
TooBug <i@toobug.net>
2012-06-08 14:06:28 +0800

    播放时可带参数决定是否循环、音量

--- src/jSound.js
+++ src/jSound.js
@@ -6,19 +6,40 @@
 			setTimeout(arguments.callee,50);
 			return false;
 		}
-		console.log('init');
 		var elem = document.createElement('audio'),src;
 		if (!elem.canPlayType){
 			elem = document.createElement('bgsound');
 		}
 		document.body.appendChild(elem);
 		w.jSound={
-			play:function(soundSrc){
-				src=soundSrc;
-				elem.src='';
-				elem.src=src;
+			play:function(ops){
+				var options;
+				if (typeof ops === 'string'){
+					options = {
+						src : ops
+					}
+				}else{
+					options = ops;
+				}
+				if (options.loop){
+					elem.loop = elem.canPlayType?true:-1;
+				}else{
+					elem.loop = elem.canPlayType?false:1;
+				}
+				if (options.volume !== undefined){
+					//console.log(options.volume);
+					elem.volume = elem.canPlayType?options.volume/100:-(options.volume-100)*(options.volume-100);
+				}
+				elem.src=options.src;
 				if (elem.canPlayType){
 					elem.play();
+				}
+			},
+			stop:function(){
+				if (elem.canPlayType){
+					elem.pause();
+				}else{
+					elem.src='';
 				}
 			}
 		}
