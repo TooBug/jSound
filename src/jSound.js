5d20d416ae1ea7b500c0ddc9a7b34480c494e5d7
TooBug <i@toobug.net>
2013-10-05 11:46:37 +0800

    优化代码结构

--- src/jSound.js
+++ src/jSound.js
@@ -1,65 +1,78 @@
-/**
+/*!
  * jSound 0.0.1 preview
  * @author TooBug <i@toobug.net>
  * @license MIT
-*/
+ */
+
 (function(w){
+
 	'use strict';
-	if (!w.jSound){
-		w.jSound = {};
-	}
+
+	var jSound = {};
+
+	var elem;
+
 	var init = function(){
-		if(w.jSound.init)return;
-		var elem = document.createElement('audio');
+
+		elem = document.createElement('audio');
 		if (!elem.canPlayType){
 			elem = document.createElement('bgsound');
 		}
 		document.body.appendChild(elem);
-		w.jSound.play = function(ops){
-			var options;
-			if (typeof ops === 'string'){
-				options = {
-					src : ops
-				};
-			}else{
-				options = ops;
-			}
-			if (options.loop){
-				elem.loop = elem.canPlayType?true:-1;
-			}else{
-				elem.loop = elem.canPlayType?false:1;
-			}
-			if (options.volume !== undefined){
-				elem.volume = elem.canPlayType?options.volume/100:-(options.volume-100)*(options.volume-100);
-			}
-			elem.src=options.src;
-			if (elem.canPlayType){
-				elem.play();
-			}
-		};
-		w.jSound.stop = function(){
-			if (elem.canPlayType){
-				elem.pause();
-			}else{
-				elem.src='';
-			}
-		};
-		w.jSound.playTone = function(toneName){
-			elem.src='data:audio/wav;base64,'+this.sounds[toneName];
-			if (elem.canPlayType){
-				elem.play();
-			}
-		};
-		w.jSound.init = 1;
+	};
+	
+	/**
+	 * 播放音效
+	 * @param {String} src 要播放的音频文件URL
+	 * @param {Object} ops 播放设置
+	 * @param {Boolean} ops.loop 是否循环播放
+	 * @param {Number} ops.volume 音量大小，取值为0-100
+	 * @return {undefined}
+	 */
+	jSound.play = function(src,ops){
+		var options;
+
+		// 如果只传了ops，没有传src
+		if (typeof src === 'object'){
+			options = src;
+		}else{
+			options = ops || {};
+			options.src = src;
+		}
+		if (options.loop){
+			elem.loop = elem.canPlayType?true:-1;
+		}else{
+			elem.loop = elem.canPlayType?false:1;
+		}
+		if (options.volume !== undefined){
+			elem.volume = elem.canPlayType?options.volume/100:-(options.volume-100)*(options.volume-100);
+		}
+		elem.src=options.src;
+		if (elem.canPlayType){
+			elem.play();
+		}
+	};
+	jSound.stop = function(){
+		if (elem.canPlayType){
+			elem.pause();
+		}else{
+			elem.src='';
+		}
+	};
+	jSound.playTone = function(toneName){
+		elem.src='data:audio/wav;base64,'+this.sounds[toneName];
+		if (elem.canPlayType){
+			elem.play();
+		}
 	};
 
 	//detect DOM Ready Event
 	if (window.addEventListener){
-		w.jSound.domReady = function(readyFunc){
+		jSound.domReady = function(readyFunc){
 			document.addEventListener('DOMContentLoaded',readyFunc,false);
 		};
 	}else if (window.attachEvent){
-		w.jSound.domReady = function(readyFunc){
+		jSound.domReady = function(readyFunc){
 			document.attachEvent('onreadystatechange',function(){
 				if (document.readyState === 'complete'){
 					readyFunc();
@@ -68,8 +81,10 @@
 		};
 	}
 
-	w.jSound.domReady(function(){
+	jSound.domReady(function(){
 		init(w);
 	});
+
+	w.jSound = jSound;
 
 })(window);
